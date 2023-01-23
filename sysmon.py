96faaa604946d65e29375fff27e59d5334e04636
PolGs <polgalvezsoriano@gmail.com>
2023-01-23 16:21:23 +0000

    Add files via upload

--- sysmon.py
+++ sysmon.py
@@ -55,5 +55,12 @@ def login():
     else:
         return jsonify(msg='Invalid key'), 401
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
